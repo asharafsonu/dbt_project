31cfa5700a4fd8cfacad75b3ce2a0068dd6d3218
erasharaf143@gmail.com <erasharaf143@gmail.com>
2024-10-30 07:08:27 +0000

    commit

---
+++ models/emp_details_model.sql
@@ -0,0 +1,1 @@
+select * from dbt_db.dbt_schema.emp_details
--- models/staging_data_model.sql
+++ models/staging_data_model.sql
@@ -1,4 +1,5 @@
 -- models/staging_data_model.sql
-
-SELECT * 
-FROM dbt_db.dbt_schema.staging_data
+create table emp_details_final as(
+SELECT  sd.id,sd.name,ed.domain,ed.location
+FROM dbt_db.dbt_schema.emp_details ed
+join dbt_db.dbt_schema.staging_data sd on ed.id=sd.id)
