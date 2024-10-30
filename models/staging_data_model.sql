2a6f856da0ee6a016de6988cd2b01cf02580ba31
Mohammad Asharaf <erasharaf143@gmail.com>
2024-10-30 08:54:56 +0000

    Commit

--- models/staging_data_model.sql
+++ models/staging_data_model.sql
@@ -1,5 +1,4 @@
 -- models/staging_data_model.sql
-create table emp_details_final as(
 SELECT  sd.id,sd.name,ed.domain,ed.location
 FROM dbt_db.dbt_schema.emp_details ed
-join dbt_db.dbt_schema.staging_data sd on ed.id=sd.id)
+join dbt_db.dbt_schema.staging_data sd on ed.id=sd.id
